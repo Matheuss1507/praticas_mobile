1fcd51a63276d9a9a681f68f36227437455d7ec2
Matheus Soares <70666128+Matheuss1507@users.noreply.github.com>
2026-05-16 12:03:12 -0300

    Prática 3 - Parte 1

---
+++ app/src/main/java/com/weatherapp/City.kt
@@ -0,0 +1,8 @@
+package com.weatherapp
+
+data class City (
+    val name : String,
+    val weather: String? = null,
+    val location: String? = null
+)
+
--- app/src/main/java/com/weatherapp/ui/ListPage.kt
+++ app/src/main/java/com/weatherapp/ui/ListPage.kt
@@ -1,9 +1,23 @@
 package com.weatherapp.ui
 
+import android.app.Activity
+import android.content.Intent
+import android.content.Intent.FLAG_ACTIVITY_SINGLE_TOP
+import android.widget.Toast
+import androidx.activity.compose.LocalActivity
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.rounded.FavoriteBorder
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
@@ -11,23 +25,62 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.weatherapp.City
+import androidx.compose.foundation.lazy.items
+import com.weatherapp.MainActivity
 
+private fun getCities() = List(20) { i ->
+    City(name = "Cidade $i", weather = "Carregando clima...")
+}
 @Preview(showBackground = true)
 @Composable
 fun ListPage(modifier: Modifier = Modifier) {
-
-    Column(
-        modifier = modifier.fillMaxSize().background(Color.Magenta)
-            .wrapContentSize(Alignment.Center)
+    val cityList = remember { getCities().toMutableStateList() }
+    val activity = LocalActivity.current as Activity // Para os Toasts
+    LazyColumn(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(8.dp)
     ) {
-        Text(
-            text = "Favoritas",
-            fontWeight = FontWeight.Bold,
-            color = Color.White,
-            modifier = modifier.align(CenterHorizontally),
-            textAlign = TextAlign.Center,
-            fontSize = 20.sp
+        items(cityList, key = { it.name }) { city ->
+            CityItem(city = city, onClose = {
+                Toast.makeText(activity, "Cidade excluída", Toast.LENGTH_LONG).show()
+            }, onClick = {
+                Toast.makeText(activity, "Cidade carregada", Toast.LENGTH_LONG).show()
+            })
+        }
+    }
+}
+
+
+@Composable
+fun CityItem(
+    city: City,
+    onClick: () -> Unit,
+    onClose: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier.fillMaxWidth().padding(8.dp).clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            Icons.Rounded.FavoriteBorder,
+            contentDescription = ""
         )
+        Spacer(modifier = Modifier.size(12.dp))
+        Column(modifier = modifier.weight(1f)) {
+            Text(modifier = Modifier,
+                text = city.name,
+                fontSize = 24.sp)
+            Text(modifier = Modifier,
+                text = city.weather?:"Carregando clima...",
+                fontSize = 16.sp)
+        }
+        IconButton(onClick = onClose) {
+            Icon(Icons.Filled.Close, contentDescription = "Close")
+        }
     }
 }
