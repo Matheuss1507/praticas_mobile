30e6d356fc1c02239d6f8e00073963f3b197b1f3
Matheus Soares <70666128+Matheuss1507@users.noreply.github.com>
2026-05-30 00:02:31 -0300

    Prática 6 - Parte 2

--- app/src/main/java/com/weatherapp/model/MainViewModel.kt
+++ app/src/main/java/com/weatherapp/model/MainViewModel.kt
@@ -1,5 +1,6 @@
 package com.weatherapp.model
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
 import com.google.android.gms.maps.model.LatLng
@@ -15,8 +16,13 @@ class MainViewModel : ViewModel() {
         _cities.add(City(name = name, location = location))
     }
 
+    private val _user = mutableStateOf<User?> (null)
+    val user : User?
+        get() = _user.value
+
 }
 
 private fun getCities() = List(20) { i ->
     City(name = "Cidade $i", weather = "Carregando clima...")
 }
+
